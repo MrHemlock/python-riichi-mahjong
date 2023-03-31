54750a4638534315148fa7fbb477b58ec1775121
Daniel J Brown <browndj3@gmail.com>
2023-03-31 15:27:12 -0500

    Changes in prep for hand checking
    - Added string dunders for easier hand checking during testing
    
    - Added additional dunders for tile comparison
    
    - Swapped Flag for Enum for I'm not sure why

--- src/tile_generation.py
+++ src/tile_generation.py
@@ -1,28 +1,38 @@
+from __future__ import annotations
+
 from dataclasses import dataclass, field
-from enum import Flag, auto
+from enum import Enum, auto
 from itertools import count
 
 
-class TileTypes(Flag):
+class TileTypes(Enum):
     DOTS = auto()
     STICKS = auto()
     CRACKS = auto()
     DRAGON = auto()
     WIND = auto()
-    HONORS = DRAGON | WIND
+
+    def __str__(self):
+        return f'{self.name}'.capitalize()
 
 
-class DragonValues(Flag):
+class DragonValues(Enum):
     GREEN = auto()
     RED = auto()
     WHITE = auto()
 
+    def __str__(self):
+        return f'{self.name}'.capitalize()
 
-class WindValues(Flag):
+
+class WindValues(Enum):
     NORTH = auto()
     WEST = auto()
     SOUTH = auto()
     EAST = auto()
+
+    def __str__(self):
+        return f'{self.name}'.capitalize()
 
 
 @dataclass(slots=True)
@@ -32,18 +42,25 @@ class Tile:
     _id: int = field(default_factory=count().__next__)
     red: bool = False
 
-    def __lt__(self, other):
+    def __lt__(self, other: Tile):
         return self._id < other._id
 
+    def __eq__(self, other: Tile):
+        return (self.suit, self.value) == (other.suit, other.value)
+
+    def __str__(self):
+        if self.suit in (TileTypes.DRAGON, TileTypes.WIND):
+            return f'{self.value} {self.suit}'.capitalize()
+        return f'{self.value} of {self.suit}'.capitalize()
 
 
 def initialize_tiles():
     all_tiles = []
-    for tile_type in TileTypes:
-        if tile_type & TileTypes.DRAGON:
+    for tile_type in (TileTypes.DRAGON, TileTypes.WIND):
+        if tile_type == TileTypes.DRAGON:
             for color in DragonValues:
                 all_tiles.extend([Tile(tile_type, color) for _ in range(4)])
-        elif tile_type & TileTypes.WIND:
+        elif tile_type == TileTypes.WIND:
             for direction in WindValues:
                 all_tiles.extend([Tile(tile_type, direction) for _ in range(4)])
         else:
